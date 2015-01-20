c8fd7bf6056bdc209e4e1e15b565aa4c74ded718
Cyril Courtiat <ccy@free.fr>
2015-01-20 19:30:46 +0100

    R 2.1.42-b: Now trace errors if compiled with DEBUG.

--- api/xfreq-api.h
+++ api/xfreq-api.h
@@ -13,13 +13,17 @@
 
 #define _MAJOR   "2"
 #define _MINOR   "1"
-#define _NIGHTLY "42-a"
+#define _NIGHTLY "42-b"
 #define AutoDate _APPNAME" "_MAJOR"."_MINOR"-"_NIGHTLY" (C) CYRIL INGENIERIE "__DATE__"\n"
 
 #define	ToStr(_inst)	_ToStr(_inst)
 #define	_ToStr(_inst)	#_inst
 
-#define	tracerr(anystr)	fprintf(stderr, "%s\n", anystr);
+#if defined(DEBUG)
+	#define	tracerr(anystr)	fprintf(stderr, "%s\n", anystr);
+#else
+	#define	tracerr(anystr)
+#endif
 
 #if defined(Linux)
 #define MAX(M, m)	((M) > (m) ? (M) : (m))
